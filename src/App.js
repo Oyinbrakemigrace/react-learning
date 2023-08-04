81a0f6edb6bbcf09102a398b729a61c43db57a64
Grace <graceofubu04@gmail.com>
2023-08-04 20:50:07 +0400

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from "react";
 import "./App.css";
 // import TheUseReducerHook from "./HooksLearning/theUseEffectHook";
-import TheUseEffectHook from "./HooksLearning/theUseEffectHook";
+// import TheUseEffectHook from "./HooksLearning/theUseEffectHook";
 // import Login from "./Components/Login";
 // import Profile from "./Components/Profile";
 // import ChangeColor from "./Components/ChangeColor";
 // import ChangeProfile from "./Components/ChangeProfile";
 //import GameBoard from "./Components/GameBoard";
+import TheUseRefHook from "./HooksLearning/theUseRefHook";
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Profile />
-      <Login />
-      <ChangeColor/>
-      <ChangeProfile/> */}
-      {/* <GameBoard/> */}
-      <TheUseEffectHook />
+      <TheUseRefHook />
     </div>
   );
 }
---
+++ src/HooksLearning/theUseRefHook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TheUseRefHook = () => {
+  return (
+    <div>theUseRefHook</div>
+  )
+}
+
+export default TheUseRefHook
