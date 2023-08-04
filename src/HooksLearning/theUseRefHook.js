703f4cd366e5f0b548ea7cb365b7a7efc46d707c
Grace <graceofubu04@gmail.com>
2023-08-04 21:04:23 +0400

    useRef hook

--- src/HooksLearning/theUseRefHook.js
+++ src/HooksLearning/theUseRefHook.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
 const TheUseRefHook = () => {
+  const inputRef = useRef(null)
+  const onclick = ()=>{
+    //console.log(inputRef.current.value)
+    inputRef.current.value = ''
+  }
   return (
-    <div>theUseRefHook</div>
+    <div>
+      <h1>GRACE</h1>
+      <input type="text" ref={inputRef}/>
+      <button onClick={onclick}>Change Name</button>
+    </div>
   )
 }
 
